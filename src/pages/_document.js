f4d1236a7b9cc325e7b215359900a5b911548ea9
Filipe Batista <filipe.x@live.com>
2021-03-29 11:01:49 -0300

    adicionando a tag do google analytics

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -11,8 +11,16 @@ class CustomDocument extends Document {
       <Html lang="pt-br">
         <Head>
           <link rel="stylesheet" href="/styles.css" />
+
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-21W1RKR7EV"></script>
           <script dangerouslySetInnerHTML={{
             __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'G-21W1RKR7EV');
+
                 (function(w,d,s,l,i){w[l] = w[l] || []{'gtm.start':
                 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
