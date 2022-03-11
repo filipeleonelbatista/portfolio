8982b49b599d1175ca720af12c9b04b91ce36dda
Filipe Batista <filipe.x@live.com>
2022-03-11 15:37:53 -0300

    adiciona unique

--- src/sections/other-projects.js
+++ src/sections/other-projects.js
@@ -1,8 +1,8 @@
-import BlockTitle from 'components/block-title';
-import OtherCardProject from 'components/other-card-projects';
-import React, { useEffect, useState } from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Box, Container } from 'theme-ui';
+import BlockTitle from "components/block-title";
+import OtherCardProject from "components/other-card-projects";
+import React, { useEffect, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Box, Container } from "theme-ui";
 
 const OtherProjects = () => {
   const FavoriteCarousel = {
@@ -38,35 +38,42 @@ const OtherProjects = () => {
   const [favoriteCourseData, setFavoriteCourseData] = useState([]);
 
   async function loadItens() {
-    let newfavoriteCourseData = [      
+    let newfavoriteCourseData = [
       {
-        title: 'ClikCar RS',
-        description: 'Aluguel de carros para motoristas de aplicativo.',
-        image: `/images/other-projects/1.png`,
-        view_app_url: 'https://clikcarrs.com.br/',
+        title: "Cadastra Pet",
+        description:
+          "Mantenha os registros clinicos do seu pet em qualquer lugar.",
+        image: `/images/other-projects/3.png`,
+        view_app_url: "https://cadastrapet.co/",
       },
       {
-        title: 'Ascorsan',
-        description: 'Portal de notícias e informações para associados.',
-        image: `/images/other-projects/2.png`,
-        view_app_url: 'https://ascorsan.com.br/',
-      }, 
+        title: "Unique",
+        description:
+          "Aluguel de carros com o proprietário com contrato, GPS e seguro.",
+        image: `/images/other-projects/1.png`,
+        view_app_url: "https://aluguel-de-veiculos.vercel.app/",
+      },
       {
-        title: 'Cadastra Pet',
-        description: 'Mantenha os registros clinicos do seu pet em qualquer lugar.',
-        image: `/images/other-projects/3.png`,
-        view_app_url: 'https://cadastrapet.co/',
-      },   
+        title: "Ascorsan",
+        description: "Portal de notícias e informações para associados.",
+        image: `/images/other-projects/2.png`,
+        view_app_url: "https://ascorsan.com.br/",
+      },
+      {
+        title: "Sistema de votação - Ascorsan",
+        description:
+          "Sistema de votação para os associados com segurança e voto impresso.",
+        image: `/images/other-projects/2.png`,
+        view_app_url: "https://ascorsan.com.br/votacao",
+      },
     ];
 
-    setFavoriteCourseData(newfavoriteCourseData)
-
-
+    setFavoriteCourseData(newfavoriteCourseData);
   }
 
   useEffect(() => {
     loadItens();
-  }, [])
+  }, []);
 
   return (
     <Box as="section" id="outros-projetos" sx={styles.fevCourse}>
@@ -97,35 +104,35 @@ export default OtherProjects;
 
 const styles = {
   fevCourse: {
-    paddingTop: ['60px', null, null, '80px', '10px'],
-    pb: [null, null, null, '30px', '50px', '0'],
+    paddingTop: ["60px", null, null, "80px", "10px"],
+    pb: [null, null, null, "30px", "50px", "0"],
     container: {
       maxWidth: 1440,
-      '@media(max-width:1440px)': {
+      "@media(max-width:1440px)": {
         maxWidth: 1240,
       },
-      '.swiper-container': {
-        pb: '30px',
-        overflow: [null, null, null, null, null, 'visible'],
+      ".swiper-container": {
+        pb: "30px",
+        overflow: [null, null, null, null, null, "visible"],
       },
     },
     blockTitle: {
-      textAlign: 'center',
-      marginBottom: '30px',
+      textAlign: "center",
+      marginBottom: "30px",
     },
     row: {
-      display: 'grid',
-      gridTemplateColumns: '1fr 1fr 1fr',
+      display: "grid",
+      gridTemplateColumns: "1fr 1fr 1fr",
       gridGap: 30,
-      '@media(max-width: 991px)': {
-        gridTemplateColumns: '1fr 1fr',
+      "@media(max-width: 991px)": {
+        gridTemplateColumns: "1fr 1fr",
       },
-      '@media(max-width: 575px)': {
-        gridTemplateColumns: '1fr',
+      "@media(max-width: 575px)": {
+        gridTemplateColumns: "1fr",
       },
     },
     col: {
-      display: 'flex',
+      display: "flex",
     },
   },
 };
