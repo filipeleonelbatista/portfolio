97827dcd6962595f8023c98a6c883fbe219dcaf8
Filipe Batista <filipe.x@live.com>
2022-03-31 10:08:05 -0300

    adicionar o instafeed

--- src/components/InstaFeed.js
+++ src/components/InstaFeed.js
@@ -1,36 +1,29 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
 import styles from "../styles/components/InstaFeed.module.css";
 
 export default function InstaFeed() {
-  const [user, setUser] = useState();
   const [posts, setPosts] = useState();
 
   useEffect(() => {
-    // const executeAsync = async () => {
-    // const url = "https://www.instagram.com/filipeleonelbatista/?__a=1";
-    // const proxy = "https://cors-anywhere.herokuapp.com/";
-    // const result = await axios.get(
-    //   process.env.NODE_ENV === "development" ? proxy + url : url,
-    //   {
-    //     headers: {
-    //       "Access-Control-Allow-Origin": "*",
-    //       "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
-    //     },
-    //   }
-    // );
-    // const userInfo = {
-    //   id: result.data.graphql.user.id,
-    //   fbid: result.data.graphql.user.fbid,
-    //   full_name: result.data.graphql.user.full_name,
-    //   username: result.data.graphql.user.username,
-    //   avatar: result.data.graphql.user.profile_pic_url,
-    // };
-    // const postsArray =
-    //   result.data.graphql.user.edge_owner_to_timeline_media.edges;
-    // setUser(userInfo);
-    // setPosts(postsArray);
-    // };
-    // executeAsync();
+    //https://developers.facebook.com/docs/instagram-basic-display-api/getting-started
+    //https://developers.facebook.com/docs/instagram-basic-display-api/guides/getting-profiles-and-media
+    // Token de longa dura 60 dias
+
+    const executeAsync = async () => {
+      const url =
+        "https://graph.instagram.com/me/media?fields=id,caption,media_url,media_type,permalink,thumbnail_url,timestamp&access_token=IGQVJWTjBrVDZAIQVZAIVnluTmxqR050QzZABOTEzaG9tUnFvYVg0VE9nV1p0VWpOeEJXcG9MNHQ3N196RHYzbnlSdFRQWkM2R2RrYm84M2x6OF9NWG56R1U5RVdWSFhGQWZAPcTJyZA0R5M0ZApNU5DRTBLbHNZAaGhkaV9rWjBN";
+
+      const result = await axios.get(url);
+      const lastPosts = [
+        result.data.data[0],
+        result.data.data[1],
+        result.data.data[2],
+      ];
+
+      setPosts(lastPosts);
+    };
+    executeAsync();
   }, []);
 
   // if (!user || !posts) return null;
@@ -40,99 +33,46 @@ export default function InstaFeed() {
         <h3 className={styles.title}>Acompanhe nas redes sociais</h3>
         <p className={styles.subtitle}>Últimas postagens</p>
         <div className={styles.postList}>
-          <div className={styles.postContainer}>
-            <div
-              title={"filipeleonelbatista"}
-              style={{
-                background: `url(https://desenvolvedordeaplicativos.vercel.app/_next/static/images/banner-image-1-a66ee6f73d07bb1017577a288c87ef6a.png) no-repeat center center`,
-                width: "100%",
-                height: "20rem",
-                backgroundSize: "cover",
-              }}
-            ></div>
-            <div className={styles.postFooter}>
-              <div
-                title={"filipeleonelbatista"}
-                style={{
-                  background: `url(https://desenvolvedordeaplicativos.vercel.app/_next/static/images/banner-image-1-a66ee6f73d07bb1017577a288c87ef6a.png) no-repeat center center`,
-                  borderRadius: "50%",
-                  width: "2.4rem",
-                  height: "2.4rem",
-                  backgroundSize: "cover",
-                }}
-              ></div>
-              <div className={styles.postFooterContent}>
-                <strong className={styles.usernameFooter}>
-                  filipeleonelbatista
-                </strong>
-                <p className={styles.descriptionFooter}>
-                  Testando a postagem que ...
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className={styles.postContainer}>
-            <div
-              title={"filipeleonelbatista"}
-              style={{
-                background: `url(https://desenvolvedordeaplicativos.vercel.app/_next/static/images/banner-image-1-a66ee6f73d07bb1017577a288c87ef6a.png) no-repeat center center`,
-                width: "100%",
-                height: "20rem",
-                backgroundSize: "cover",
-              }}
-            ></div>
-            <div className={styles.postFooter}>
-              <div
-                title={"filipeleonelbatista"}
-                style={{
-                  background: `url(https://desenvolvedordeaplicativos.vercel.app/_next/static/images/banner-image-1-a66ee6f73d07bb1017577a288c87ef6a.png) no-repeat center center`,
-                  borderRadius: "50%",
-                  width: "2.4rem",
-                  height: "2.4rem",
-                  backgroundSize: "cover",
-                }}
-              ></div>
-              <div className={styles.postFooterContent}>
-                <strong className={styles.usernameFooter}>
-                  filipeleonelbatista
-                </strong>
-                <p className={styles.descriptionFooter}>
-                  Testando a postagem que ...
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className={styles.postContainer}>
-            <div
-              title={"filipeleonelbatista"}
-              style={{
-                background: `url(https://desenvolvedordeaplicativos.vercel.app/_next/static/images/banner-image-1-a66ee6f73d07bb1017577a288c87ef6a.png) no-repeat center center`,
-                width: "100%",
-                height: "20rem",
-                backgroundSize: "cover",
-              }}
-            ></div>
-            <div className={styles.postFooter}>
-              <div
-                title={"filipeleonelbatista"}
-                style={{
-                  background: `url(https://desenvolvedordeaplicativos.vercel.app/_next/static/images/banner-image-1-a66ee6f73d07bb1017577a288c87ef6a.png) no-repeat center center`,
-                  borderRadius: "50%",
-                  width: "2.4rem",
-                  height: "2.4rem",
-                  backgroundSize: "cover",
-                }}
-              ></div>
-              <div className={styles.postFooterContent}>
-                <strong className={styles.usernameFooter}>
-                  filipeleonelbatista
-                </strong>
-                <p className={styles.descriptionFooter}>
-                  Testando a postagem que ...
-                </p>
-              </div>
-            </div>
-          </div>
+          {posts &&
+            posts.map((post) => {
+              return (
+                <div
+                  key={post.id}
+                  onClick={() => window.open(post.permalink, "_blank")}
+                  className={styles.postContainer}
+                >
+                  <div
+                    title={"filipeleonelbatista"}
+                    style={{
+                      background: `url(${post.media_url}) no-repeat center center`,
+                      width: "100%",
+                      height: "20rem",
+                      backgroundSize: "cover",
+                    }}
+                  ></div>
+                  <div className={styles.postFooter}>
+                    <div
+                      title={"filipeleonelbatista"}
+                      style={{
+                        background: `url(https://desenvolvedordeaplicativos.vercel.app/_next/static/images/banner-image-1-a66ee6f73d07bb1017577a288c87ef6a.png) no-repeat center center`,
+                        borderRadius: "50%",
+                        width: "2.4rem",
+                        height: "2.4rem",
+                        backgroundSize: "cover",
+                      }}
+                    ></div>
+                    <div className={styles.postFooterContent}>
+                      <strong className={styles.usernameFooter}>
+                        filipeleonelbatista
+                      </strong>
+                      <p className={styles.descriptionFooter}>
+                        {post.caption.substr(0, 50)}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
         </div>
       </div>
     </div>
