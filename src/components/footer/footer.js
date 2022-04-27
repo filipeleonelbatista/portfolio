4555a944c4f1f4871640628bb59c9825fe93c37b
Filipe Batista <filipe.x@live.com>
2022-04-27 13:09:03 -0300

    update links

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,38 +1,98 @@
 /** @jsx jsx */
-import { FaEnvelope, FaGithub, FaInstagram, FaLinkedinIn, FaWhatsapp } from 'react-icons/fa';
-import { Container, Divider, jsx } from 'theme-ui';
+import {
+  FaEnvelope,
+  FaGithub,
+  FaInstagram,
+  FaLinkedinIn,
+  FaWhatsapp,
+} from "react-icons/fa";
+import { Container, Divider, jsx } from "theme-ui";
 
 export default function Footer() {
   return (
     <footer sx={styles.footer}>
       <Divider sx={styles.footer.divider} />
       <Container sx={styles.footer.container}>
-
-
-        <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
-          <a target="_blank" rel="noopener noreferer" href="https://github.com/filipeleonelbatista" style={{ padding: '0.8rem', border: 'solid 1px #343d48', borderRadius: '0.3rem', margin: '0.2rem', display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', color: '#343d48', svg: { width: 24, heigth: 24 }, "&:hover": { color: '#666' } }}>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          {/* <a target="_blank" rel="noopener noreferer" href="https://github.com/filipeleonelbatista" style={{ padding: '0.8rem', border: 'solid 1px #343d48', borderRadius: '0.3rem', margin: '0.2rem', display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', color: '#343d48', svg: { width: 24, heigth: 24 }, "&:hover": { color: '#666' } }}>
             <FaGithub />
-          </a>
-          <a target="_blank" rel="noopener noreferer" href="https://www.linkedin.com/in/filipeleonelbatista/" style={{ padding: '0.8rem', border: 'solid 1px #343d48', borderRadius: '0.3rem', margin: '0.2rem', display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', color: '#343d48', svg: { width: 24, heigth: 24 }, "&:hover": { color: '#666' } }}>
+          </a> */}
+          <a
+            target="_blank"
+            rel="noopener noreferer"
+            href="https://www.linkedin.com/in/filipeleonelbatista/"
+            style={{
+              padding: "0.8rem",
+              border: "solid 1px #343d48",
+              borderRadius: "0.3rem",
+              margin: "0.2rem",
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center",
+              color: "#343d48",
+              svg: { width: 24, heigth: 24 },
+              "&:hover": { color: "#666" },
+            }}
+          >
             <FaLinkedinIn />
           </a>
-          <a target="_blank" rel="noopener noreferer" href="https://www.instagram.com/filipeleonelbatista/" style={{ padding: '0.8rem', border: 'solid 1px #343d48', borderRadius: '0.3rem', margin: '0.2rem', display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', color: '#343d48', svg: { width: 24, heigth: 24 }, "&:hover": { color: '#666' } }}>
+          <a
+            target="_blank"
+            rel="noopener noreferer"
+            href="https://www.instagram.com/filipeleonelbatista/"
+            style={{
+              padding: "0.8rem",
+              border: "solid 1px #343d48",
+              borderRadius: "0.3rem",
+              margin: "0.2rem",
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center",
+              color: "#343d48",
+              svg: { width: 24, heigth: 24 },
+              "&:hover": { color: "#666" },
+            }}
+          >
             <FaInstagram />
           </a>
-          <a target="_blank" rel="noopener noreferer" href="mailto:filipe.x2016@gmail.com" style={{ padding: '0.8rem', border: 'solid 1px #343d48', borderRadius: '0.3rem', margin: '0.2rem', display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', color: '#343d48', svg: { width: 24, heigth: 24 }, "&:hover": { color: '#666' } }}>
+          <a
+            target="_blank"
+            rel="noopener noreferer"
+            href="mailto:filipe.batista@desenvolvedordeaplicativos.com.br"
+            style={{
+              padding: "0.8rem",
+              border: "solid 1px #343d48",
+              borderRadius: "0.3rem",
+              margin: "0.2rem",
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center",
+              color: "#343d48",
+              svg: { width: 24, heigth: 24 },
+              "&:hover": { color: "#666" },
+            }}
+          >
             <FaEnvelope />
           </a>
-          <a target="_blank" rel="noopener noreferer" href="https://wa.me/+5551986320477" style={{ padding: '0.8rem', border: 'solid 1px #343d48', borderRadius: '0.3rem', margin: '0.2rem', display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', color: '#343d48', svg: { width: 24, heigth: 24 }, "&:hover": { color: '#666' } }}>
+          {/* <a target="_blank" rel="noopener noreferer" href="https://wa.me/+5551986320477" style={{ padding: '0.8rem', border: 'solid 1px #343d48', borderRadius: '0.3rem', margin: '0.2rem', display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', color: '#343d48', svg: { width: 24, heigth: 24 }, "&:hover": { color: '#666' } }}>
             <FaWhatsapp />
-          </a>
+          </a> */}
         </div>
 
-        <p style={{ textAlign: 'center' }}>
-          {
-            `${new Date().getFullYear()} © Direitos reservados.`
-          }
-          <br />Desenvolvido em NextJS por Filipe Batista
-
+        <p style={{ textAlign: "center" }}>
+          {`${new Date().getFullYear()} © Direitos reservados.`}
+          <br />
+          Desenvolvido em NextJS por Filipe Batista
         </p>
 
         {/* {menuItems.map(({ header, items }, i) => (
@@ -56,52 +116,52 @@ export default function Footer() {
 
 const styles = {
   footer: {
-    paddingBottom: '60px',
-    '@media(max-width: 1024px)': {
-      paddingBottom: '70px',
+    paddingBottom: "60px",
+    "@media(max-width: 1024px)": {
+      paddingBottom: "70px",
     },
-    '@media(max-width: 991px)': {
-      paddingBottom: '30px',
+    "@media(max-width: 991px)": {
+      paddingBottom: "30px",
     },
     divider: {
-      borderColor: '#E5ECF4',
+      borderColor: "#E5ECF4",
       margin: 0,
-      borderWidth: '1px',
-      width: '100%',
-      maxWidth: '900px',
-      marginLeft: 'auto',
-      marginRight: 'auto',
-      marginBottom: '55px',
+      borderWidth: "1px",
+      width: "100%",
+      maxWidth: "900px",
+      marginLeft: "auto",
+      marginRight: "auto",
+      marginBottom: "55px",
     },
     container: {
-      width: '100%',
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      justfyContent: 'center'
+      width: "100%",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justfyContent: "center",
     },
     widget: {
-      '@media(max-width: 991px)': {
-        marginBottom: '30px',
+      "@media(max-width: 991px)": {
+        marginBottom: "30px",
       },
-      'a:hover': {
-        color: 'primary',
-        cursor: 'pointer',
+      "a:hover": {
+        color: "primary",
+        cursor: "pointer",
       },
-      'a+a': {
-        marginTop: '6px',
+      "a+a": {
+        marginTop: "6px",
       },
     },
     title: {
-      fontSize: '18px',
+      fontSize: "18px",
       fontWeight: 500,
       lineHeight: 1.67,
       margin: 0,
-      marginBottom: '14px',
+      marginBottom: "14px",
     },
     text: {
-      fontSize: '14px',
-      cursor: 'pointer',
+      fontSize: "14px",
+      cursor: "pointer",
     },
   },
 };
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,12 +1,17 @@
 /** @jsx jsx */
-import Logo from 'components/logo';
-import { DrawerProvider } from 'contexts/drawer/drawer.provider';
-import { FaEnvelope, FaGithub, FaInstagram, FaLinkedinIn, FaWhatsapp } from 'react-icons/fa';
-import { Link as ScrollLink } from 'react-scroll';
-import { Container, Flex, jsx } from 'theme-ui';
-import menuItems from './header.data';
-import MobileDrawer from './mobileDrawer';
-
+import Logo from "components/logo";
+import { DrawerProvider } from "contexts/drawer/drawer.provider";
+import {
+  FaEnvelope,
+  FaGithub,
+  FaInstagram,
+  FaLinkedinIn,
+  FaWhatsapp,
+} from "react-icons/fa";
+import { Link as ScrollLink } from "react-scroll";
+import { Container, Flex, jsx } from "theme-ui";
+import menuItems from "./header.data";
+import MobileDrawer from "./mobileDrawer";
 
 export default function Header({ className }) {
   return (
@@ -40,22 +45,80 @@ export default function Header({ className }) {
             variant="buttons.primary"
           /> */}
 
-          <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
-            <a target="_blank" rel="noopener noreferer" href="https://github.com/filipeleonelbatista" style={{ padding: '0.8rem', border: 'solid 1px #0063c6', borderRadius: '0.3rem', margin: '0.2rem', display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', color: '#0063c6', svg: { width: 24, heigth: 24 }, "&:hover": { color: '#666' } }}>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            {/* <a target="_blank" rel="noopener noreferer" href="https://github.com/filipeleonelbatista" style={{ padding: '0.8rem', border: 'solid 1px #0063c6', borderRadius: '0.3rem', margin: '0.2rem', display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', color: '#0063c6', svg: { width: 24, heigth: 24 }, "&:hover": { color: '#666' } }}>
               <FaGithub />
-            </a>
-            <a target="_blank" rel="noopener noreferer" href="https://www.linkedin.com/in/filipeleonelbatista/" style={{ padding: '0.8rem', border: 'solid 1px #0063c6', borderRadius: '0.3rem', margin: '0.2rem', display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', color: '#0063c6', svg: { width: 24, heigth: 24 }, "&:hover": { color: '#666' } }}>
+            </a> */}
+            <a
+              target="_blank"
+              rel="noopener noreferer"
+              href="https://www.linkedin.com/in/filipeleonelbatista/"
+              style={{
+                padding: "0.8rem",
+                border: "solid 1px #0063c6",
+                borderRadius: "0.3rem",
+                margin: "0.2rem",
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "center",
+                alignItems: "center",
+                color: "#0063c6",
+                svg: { width: 24, heigth: 24 },
+                "&:hover": { color: "#666" },
+              }}
+            >
               <FaLinkedinIn />
             </a>
-            <a target="_blank" rel="noopener noreferer" href="https://www.instagram.com/filipeleonelbatista/" style={{ padding: '0.8rem', border: 'solid 1px #0063c6', borderRadius: '0.3rem', margin: '0.2rem', display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', color: '#0063c6', svg: { width: 24, heigth: 24 }, "&:hover": { color: '#666' } }}>
+            <a
+              target="_blank"
+              rel="noopener noreferer"
+              href="https://www.instagram.com/filipeleonelbatista/"
+              style={{
+                padding: "0.8rem",
+                border: "solid 1px #0063c6",
+                borderRadius: "0.3rem",
+                margin: "0.2rem",
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "center",
+                alignItems: "center",
+                color: "#0063c6",
+                svg: { width: 24, heigth: 24 },
+                "&:hover": { color: "#666" },
+              }}
+            >
               <FaInstagram />
             </a>
-            <a target="_blank" rel="noopener noreferer" href="mailto:filipe.x2016@gmail.com" style={{ padding: '0.8rem', border: 'solid 1px #0063c6', borderRadius: '0.3rem', margin: '0.2rem', display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', color: '#0063c6', svg: { width: 24, heigth: 24 }, "&:hover": { color: '#666' } }}>
+            <a
+              target="_blank"
+              rel="noopener noreferer"
+              href="mailto:filipe.batista@desenvolvedordeaplicativos.com.br"
+              style={{
+                padding: "0.8rem",
+                border: "solid 1px #0063c6",
+                borderRadius: "0.3rem",
+                margin: "0.2rem",
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "center",
+                alignItems: "center",
+                color: "#0063c6",
+                svg: { width: 24, heigth: 24 },
+                "&:hover": { color: "#666" },
+              }}
+            >
               <FaEnvelope />
             </a>
-            <a target="_blank" rel="noopener noreferer" href="https://wa.me/+5551986320477" style={{ padding: '0.8rem', border: 'solid 1px #0063c6', borderRadius: '0.3rem', margin: '0.2rem', display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', color: '#0063c6', svg: { width: 24, heigth: 24 }, "&:hover": { color: '#666' } }}>
+            {/* <a target="_blank" rel="noopener noreferer" href="https://wa.me/+5551986320477" style={{ padding: '0.8rem', border: 'solid 1px #0063c6', borderRadius: '0.3rem', margin: '0.2rem', display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', color: '#0063c6', svg: { width: 24, heigth: 24 }, "&:hover": { color: '#666' } }}>
               <FaWhatsapp />
-            </a>
+            </a> */}
           </div>
 
           <MobileDrawer />
@@ -67,57 +130,57 @@ export default function Header({ className }) {
 
 const styles = {
   headerBtn: {
-    fontSize: '16px',
+    fontSize: "16px",
     fontWeight: 700,
-    backgroundColor: '#ACCEF7',
-    display: ['none', null, null, null, 'inline-block'],
+    backgroundColor: "#ACCEF7",
+    display: ["none", null, null, null, "inline-block"],
   },
   header: {
-    color: 'text_white',
-    fontWeight: 'normal',
-    py: '25px',
-    width: '100%',
-    position: 'fixed',
+    color: "text_white",
+    fontWeight: "normal",
+    py: "25px",
+    width: "100%",
+    position: "fixed",
     top: 0,
     left: 0,
-    backgroundColor: 'transparent',
-    transition: 'all 0.4s ease',
-    '&.sticky': {
-      backgroundColor: 'background',
-      color: 'text',
-      py: '15px',
-      boxShadow: '0 1px 2px rgba(0, 0, 0, 0.06)',
+    backgroundColor: "transparent",
+    transition: "all 0.4s ease",
+    "&.sticky": {
+      backgroundColor: "background",
+      color: "text",
+      py: "15px",
+      boxShadow: "0 1px 2px rgba(0, 0, 0, 0.06)",
     },
   },
   container: {
-    display: 'flex',
-    alignItems: 'center',
-    maxWidth: '1430px',
-    '@media(max-width:1440px)': {
-      maxWidth: '1230px',
+    display: "flex",
+    alignItems: "center",
+    maxWidth: "1430px",
+    "@media(max-width:1440px)": {
+      maxWidth: "1230px",
     },
-    '@media screen and (max-width: 991px)': {
-      justifyContent: 'space-between',
+    "@media screen and (max-width: 991px)": {
+      justifyContent: "space-between",
     },
   },
   nav: {
-    mx: 'auto',
-    '@media screen and (max-width: 991px)': {
-      display: 'none',
+    mx: "auto",
+    "@media screen and (max-width: 991px)": {
+      display: "none",
     },
     navLink: {
-      fontSize: '16px',
-      color: '#02073E',
-      fontWeight: '400',
-      cursor: 'pointer',
-      lineHeight: '1.2',
-      mr: '48px',
-      transition: '500ms',
-      ':last-child': {
-        mr: '0',
+      fontSize: "16px",
+      color: "#02073E",
+      fontWeight: "400",
+      cursor: "pointer",
+      lineHeight: "1.2",
+      mr: "48px",
+      transition: "500ms",
+      ":last-child": {
+        mr: "0",
       },
-      '&:hover, &.active': {
-        color: 'primary',
+      "&:hover, &.active": {
+        color: "primary",
       },
     },
   },
