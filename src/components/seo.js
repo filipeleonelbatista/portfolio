a82569d07e66f041020269f23d3833baa65c4eab
Filipe Batista <filipe.x@live.com>
2022-05-02 08:58:41 -0300

    addSEO

--- src/components/seo.js
+++ src/components/seo.js
@@ -69,9 +69,83 @@ export default function SEO({
   return (
     <Head>
       <title>{title}</title>
-      {metaData.map(({ name, content }, i) => (
+      {/* SEO TRADICIONAL */}
+      <meta
+        name="title"
+        content="Filipe Batista | Desenvolvedor de aplicativos"
+      />
+      <meta name="robots" content="index" />
+      <meta
+        name="keywords"
+        content="portfólio, desenvolvedor, aplicação, aplicativos, react js, react native, nodejs, javascript, desenvolvedor de aplicativos, apk, ipa, android, ios, iphone, aplicativo, fullstack, postgres, sql, oracle"
+      />
+
+      <meta
+        name="description"
+        content="Desenvolvedor de aplicativos com foco em desenvolvimento web e mobile com experiência na stack JavaScript, ReactJS, React Native e NodeJS. Tire suas idéias do papel!"
+      />
+      <link rel="canonical" href="https://desenvolvedordeaplicativos.com.br" />
+      <meta
+        name="author"
+        content="Filipe Batista | Desenvolvedor de aplicativos"
+      />
+      <meta name="robots" content="index" />
+
+      <meta
+        itemProp="name"
+        content="Filipe Batista | Desenvolvedor de aplicativos"
+      />
+      <meta
+        itemProp="description"
+        content="Desenvolvedor de aplicativos com foco em desenvolvimento web e mobile com experiência na stack JavaScript, ReactJS, React Native e NodeJS. Tire suas idéias do papel!"
+      />
+      <meta itemProp="image" content="/images/header.png" />
+
+      <meta
+        property="og:title"
+        content="Filipe Batista | Desenvolvedor de aplicativos"
+      />
+      <meta
+        property="og:description"
+        content="Desenvolvedor de aplicativos com foco em desenvolvimento web e mobile com experiência na stack JavaScript, ReactJS, React Native e NodeJS. Tire suas idéias do papel!"
+      />
+      <meta
+        property="og:url"
+        content="https://desenvolvedordeaplicativos.com.br"
+      />
+      <meta
+        property="og:site_name"
+        content="Filipe Batista | Desenvolvedor de aplicativos"
+      />
+      <meta property="og:type" content="website" />
+      <meta property="og:image" content="/images/header.png" />
+
+      <meta
+        name="twitter:title"
+        content="Filipe Batista | Desenvolvedor de aplicativos"
+      />
+      <meta
+        name="twitter:description"
+        content="Desenvolvedor de aplicativos com foco em desenvolvimento web e mobile com experiência na stack JavaScript, ReactJS, React Native e NodeJS. Tire suas idéias do papel!"
+      />
+      <meta
+        name="twitter:url"
+        content="https://desenvolvedordeaplicativos.com.br"
+      />
+      <meta
+        name="twitter:site"
+        content="https://desenvolvedordeaplicativos.com.br"
+      />
+      <meta
+        name="twitter:creator"
+        content="Filipe Batista | Desenvolvedor de aplicativos"
+      />
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:image" content="/images/header.png" />
+
+      {/* {metaData.map(({ name, content }, i) => (
         <meta key={i} name={name} content={content} />
-      ))}
+      ))} */}
     </Head>
   );
 }
