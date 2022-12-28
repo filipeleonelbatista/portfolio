14b3300e7592ead5ca335d1b35efc8ebbc07c7ff
Filipe Batista <filipe.x@live.com>
2022-12-28 11:42:21 -0300

    update video

--- src/pages/links.js
+++ src/pages/links.js
@@ -172,7 +172,7 @@ export default function Links() {
           </div>
           <div className={styles.divider}></div>
           <iframe className={styles.videoFrame}
-            src="https://www.youtube.com/embed/CtURxtIMBVY"
+            src="https://www.youtube.com/embed/uTW1Qtw0eww"
             title="YouTube video player"
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
