57122f4e9474aa38d0abe025e00fb225bcf2d99d
Filipe Batista <filipe.x@live.com>
2024-02-19 14:14:39 -0300

    update fetch last video

--- src/components/last-video.js
+++ src/components/last-video.js
@@ -1,25 +1,18 @@
 import { useEffect, useState } from "react";
 import styles from "../styles/pages/Links.module.css";
 import axios from "axios";
-import xml2js from 'xml-js';
 
-export default function LastVideo() {
-  const channelID = "UCYUeJiqZCXcABWukG9RvQtw";
-  const youtubeRSS = `${process.env.NODE_ENV === 'development' ? "http://localhost:8080/" : ""}https://www.youtube.com/feeds/videos.xml?channel_id=${channelID}`;
+function LastVideo() {
 
-  const [lastVideoId, setLastVideoId] = useState('7VyGh_i29cE');
+  const [lastVideoId, setLastVideoId] = useState(null);
 
   useEffect(() => {
-    console.log(process.env.NODE_ENV)
     const executeAsync = async () => {
       try {
-        const { data } = await axios.get(youtubeRSS);
-        const jsonData = xml2js.xml2json(data, { compact: true, spaces: 4 });
-        const videoData = JSON.parse(jsonData.replace("JSON do feed XML do YouTube: ", ""));
-        console.log(videoData.feed.entry[0]["yt:videoId"]._text);
-        setLastVideoId(videoData.feed.entry[0]["yt:videoId"]._text);
+        const { data } = await axios.get(`${process.env.NODE_ENV === 'development' ? "http://localhost:3000" : "https://filipeleonelbatista.vercel.app"}/api/fetch-youtube`);
+        setLastVideoId(data.lastVideoId);
       } catch (error) {
-        console.error("Erro ao buscar dados do feed XML do YouTube:", error);
+        console.error("Erro:", error);
       }
     }
     executeAsync();
@@ -28,7 +21,7 @@ export default function LastVideo() {
   return (
     <iframe
       className={styles.videoFrame}
-      src={`https://www.youtube.com/embed/${lastVideoId}`}
+      src={`https://www.youtube.com/embed/${lastVideoId ?? '7VyGh_i29cE'}`}
       title="YouTube video player"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowFullScreen
@@ -38,3 +31,5 @@ export default function LastVideo() {
     ></iframe>
   )
 }
+
+export default LastVideo;
---
+++ src/pages/api/fetch-youtube.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+import xml2js from "xml-js";
+
+export default async function handler(req, res) {
+  if (req.method === 'GET') {
+    console.log("Cheguei")
+    const channelID = "UCYUeJiqZCXcABWukG9RvQtw";
+    const youtubeRSS = `https://www.youtube.com/feeds/videos.xml?channel_id=${channelID}`;
+
+    const { data } = await axios.get(youtubeRSS);
+    const jsonData = xml2js.xml2json(data, { compact: true, spaces: 4 });
+    const videoData = JSON.parse(jsonData.replace("JSON do feed XML do YouTube: ", ""));
+
+    res.status(200).json({ lastVideoId: videoData.feed.entry[0]["yt:videoId"]._text });
+  }
+}
